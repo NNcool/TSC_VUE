b6de86789051b58b16438d02660540062a26f5ea
zhaoli <3030486865@qq.com>
2023-11-03 16:19:13 +0800

    给知识关联页面添加地区树展示

--- TSC_VUE/src/views/relative.vue
+++ TSC_VUE/src/views/relative.vue
@@ -1,21 +1,51 @@
 <template>
     <div>
-        <el-container>
-            <el-header>
-                <home-header />
-            </el-header>
-            <el-main>知识关联页面</el-main>
-        </el-container>
+      <el-container>
+        <el-header>
+            <home-header />
+        </el-header>
+
+        <el-main>知识关联页面
+            <el-tree :data="areas" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        </el-main>
+      </el-container>
     </div>
 </template>
   
-<script>
+  <script>
+  import HomeHeader from '../components/HomeHeader.vue'
+  import axios from 'axios';
 
-import HomeHeader from '../components/HomeHeader.vue'
-export default {
+  export default {
     components: { HomeHeader },
-
-}
-</script>
+    data() {
+      return {
+        areas: [],
+        defaultProps: {
+          children: 'childrenArea',
+          label: 'areaName'
+        }
+      }
+    },
+    created() {
+        this.getAreas(); // 调用 getAreas 方法
+    },
+    methods: {
+        handleNodeClick(data) {
+            console.log(data);
+        },
+        getAreas: function () {
+            axios.get("http://localhost:9001/area/buildAreaTree")
+            .then((response) => {
+                this.areas = response.data.result;
+                console.log(this.areas)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    }
+  }; 
+  </script>
   
-<style scoped></style>
+  <style scoped></style>
