d697a03f0ed2e10f654c07084dc1cb68dc697f6e
zhaoli <3030486865@qq.com>
2023-11-09 15:25:53 +0800

    refactor：修改项目名称

--- TSC_VUE/src/main.js
+++ TSC_VUE/src/main.js
@@ -5,12 +5,19 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import App from "./App";
 import router from "./router";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:9001" // 后端的地址
+  // 其他配置选项
+});
+
+Vue.prototype.$axios = axiosInstance;
 
 /* eslint-disable no-new */
 new Vue({
-  router,
-  render: h => h(App)
+  router,
+  render: h => h(App)
 }).$mount("#app");
