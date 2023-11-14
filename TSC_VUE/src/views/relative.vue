3ccf9852cda9afcd22edb881c0bf60ee4d03c50f
zhaoli <3030486865@qq.com>
2023-11-14 17:04:23 +0800

    feat：添加地质年龄关联展示

--- TSC_VUE/src/views/relative.vue
+++ TSC_VUE/src/views/relative.vue
@@ -8,28 +8,31 @@
         <el-main>
           <el-container  style="width: 100%;">
 
-            <el-aside style="flex: 0 0 30%; max-width: 40%; background-color: rgb(238, 241, 246)">
+            <el-aside style="flex: 0 0 30%; max-width: 20%; background-color: rgb(238, 241, 246)">
               <el-scrollbar wrap-class="scroll-wrapper">
                 <div class="content-container">
                   知识关联页面
                   <el-tree :data="areas" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
                   <div>
-                    <h3>点击的叶子节点列表：</h3>
+                    <h3>当前点击的地区岩性列表：</h3>
                     <ul>
-                      <li v-for="leaf in clickedLeaves" :key="leaf.id" @click="handleOutputClick(leaf)" class="clickable-item">{{ leaf.lithologyPattern }}</li>
+                      <li v-for="leaf in clickedLeaves" :key="leaf.id" @click="handleOutputClick(leaf)" class="clickable-item">{{ leaf.lithologyPattern+"("+leaf.age+")" }}</li>
                     </ul>
                   </div>
                 </div>
               </el-scrollbar>
             </el-aside>
 
-            <el-container style="width: 40%;">
+            <el-container style="width: 70%;">
+              
+                <div class="showBox"></div>
+                <div ref="myChart" :style="{width: '80vw', height: '100vh'}" style="margin: 0 auto;"></div>
               
             </el-container>
 
-            <el-aside style="flex: 0 0 30%; max-width: 40%; background-color: rgb(238, 241, 246)">
+            <!-- <el-aside style="flex: 0 0 30%; max-width: 40%; background-color: rgb(238, 241, 246)">
               右边
-            </el-aside>
+            </el-aside> -->
           </el-container>
         </el-main>
       </el-container>
@@ -53,13 +56,14 @@
           children: 'childrenArea',
           label: 'areaName'
         },
-        clickedLeaves: []  // 用于存储点击的叶子节点信息
+        clickedLeaves: [],  // 用于存储点击的叶子节点信息
+        arr:{},
+        spotId:null,
       }
     },
 
     created() {
         this.getAreas(); // 调用 getAreas 方法
-        this.fetchMindmapData();
     },
 
     methods: {
@@ -82,37 +86,166 @@
             resolve(data);
           }, 500);
         },
+        // 二、每次点击树的节点进行调佣
         handleNodeClick(data) {
-            console.log(data);
+            console.log("当前地区节点数据：",data);
             if (data.leaf) { // 检查是否为叶子节点
                 this.getOutputs(data) // 将叶子节点信息添加到数组中
             }
         },
+        //四、点击其中一个岩性，展示其节点信息的名称和地质年龄
         handleOutputClick(leaf) {
-            console.log(leaf);
-            console.log(leaf.id);
+            console.log("点击的岩性id",leaf.id);
+            // 构建echarts
+            this.drawLine(leaf.id)
            
         },
+        // 三、如果是地区叶子节点，展示其下面岩性信息
         getOutputs: function (data) {
             axios.get("http://localhost:9001/output/findsByAreaId/"+data.id)
             .then((response) => {
                 this.clickedLeaves = response.data.result;
-                console.log(this.clickedLeaves)
+                console.log("点击叶子结点下的岩性列表：",this.clickedLeaves)
             })
             .catch((error) => {
                 console.log(error);
             });
         },
+
+        // 一、获取地区树
         getAreas: function () {
             axios.get("http://localhost:9001/area/buildAreaTree")
             .then((response) => {
                 this.areas = response.data.result;
-                console.log(this.areas)
+                console.log("当前树：",this.areas)
             })
             .catch((error) => {
                 console.log(error);
             });
-        }
+        },
+
+        drawLine(id) {
+          axios.get("http://localhost:9001/output/findOutputAndRelativeById/"+id)
+          .then((response) => {
+                this.arr = response.data.result;
+                // 数据加载完成后进行 ECharts 初始化和绘制
+                this.initECharts();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },
+
+        initECharts(){
+          var data = this.arr;
+                        var echarts = require('echarts');
+                        let imgs = 'el-icon-star-on'
+                        var myChart = echarts.init(this.$refs.myChart);
+                        var option = {
+                            backgroundColor: '#AB9793',
+                            tooltip: {
+                                show: false,
+                                trigger: 'item',
+                                triggerOn: 'mousemove'
+                            },
+                            series: [{
+                                type: 'tree',
+                                id: 0,
+                                name: 'tree1',
+                                data: [data],
+                                layout: 'orthogonal',
+
+                                top: '10%',
+                                left: '8%',
+                                bottom: '22%',
+                                right: '20%',
+
+
+                                symbolSize: 7,
+
+                                edgeShape: 'polyline',
+                                edgeForkPosition: '63%',
+                                initialTreeDepth: 0, //初始展开的层级（深度）
+
+                                //小圆点
+                                itemStyle: {
+                                    color: '#EE5A0B',
+                                    borderColor: '#EE5A0B',
+                                },
+                    
+                                leaves: {
+                                  label: {
+                                      position: 'inside',
+                                      verticalAlign: 'middle',
+                                      align: 'center',
+                                      // backgroundColor: '#0000FF',  // 设置叶子节点的背景颜色
+                                      // color: '#FFFFFF',  // 设置叶子节点文本的颜色
+                                  }
+                                },
+
+                                lineStyle: {
+                                    curveness: 0.5, //线的曲度
+                                    width: 3 ,//线宽
+                                    color : '#4B3C36',
+                                    borderColor:'#0000ff'
+
+                                },
+
+                                label: {
+                                    backgroundColor: '#A86045',
+                                    position: 'inside',
+                                    verticalAlign: 'middle',
+                                    align: 'center',
+                                    borderColor: '#4B3C36',
+                                    borderWidth: 2,
+                                    borderRadius: 5,
+                                    padding: 5,
+                                    height: 60,
+                                    lineHeight:60,
+                                    formatter: function(data) {
+                                        return ["{img|}{name| " + data.data.name + " }{value| " +data.data.age+" }"].join(' ');
+                                    },
+                                    rich: { //给不同的数据应用不同的样式
+                                        img:{
+                                            align: 'left',
+                                            backgroundColor: {
+                                                image: '../assets/Help.png'
+                                            },
+                                                            height:40
+                                        },
+                                        name: {
+                                            color: '#000',
+                                            fontSize: 14,
+                                            lineHeight: 20,
+                                            
+                                        },
+                                        value: {
+                                            color: '#000',
+                                            fontSize: 14,
+                                            lineHeight: 20,
+                                            fontWeight: 'bold',
+                                        },
+
+                                    }
+                                },
+
+                                expandAndCollapse: true,
+                                animationDuration: 550,
+                                animationDurationUpdate: 750,
+                                roam: true, //是否开启鼠标缩放和平移漫游    如果版本太低的话  这里是不生效的   我用的是4.2.1
+                                children: 'children', // 添加 children 配置
+                            }]
+                        };
+
+
+                        // 使用刚指定的配置项和数据显示图表。
+                        myChart.setOption(option);
+                        myChart.on('click',  function(param) {
+                            //这个params可以获取你要的图中的当前点击的项的参数
+                            console.log("点击")
+                            console.dir(param.data)
+                        });    
+        },
     }
   }; 
   </script>
